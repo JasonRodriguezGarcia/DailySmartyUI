ceebbf9b215af3a7696678a0d389b1c580c9db05
JasonRodriguezGarcia <jasonrodriguezempleo.gmail.com>
2024-06-19 22:09:17 +0200

    Implementing an HOC Called with Router

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from "redux-form";
+//HOC to use DOM history
+import { withRouter } from 'react-router-dom'; 
 
 class SearchBar extends Component {
 
   handleFormSubmit = function({query}) {
     console.log('trying to handle submit for query >>', query);
     // navigate to a new route
+    this.props.history.push("/results");
   }
 
   renderInput(field) {
@@ -35,4 +38,6 @@ class SearchBar extends Component {
 SearchBar = reduxForm ({
   form: "searchBar"
 }) (SearchBar)
+
+SearchBar = withRouter(SearchBar);
 export default SearchBar;
