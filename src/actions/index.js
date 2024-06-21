48a1b900b5686a80a9624848b597e6d93e92cb47
JasonRodriguezGarcia <jasonrodriguezempleo.gmail.com>
2024-06-21 16:48:31 +0200

    Utilizing Axios to Perform a GET Request on the Redux Thunk Action Dispatcher

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,21 @@
+import {SET_RECENT_POSTS} from "./types";
+import axios from "axios";
+
 export function fetchRecentPosts() {
   return function(dispatch) {
     //perform our request in here.
-    console.log("hello"); 
+//    axios.get('https://api.dailysmarty.com/posts')
+// from https://swapi.dev/
+//    axios.get('https://swapi.dev/api/')
+//    axios.get('https://swapi.dev/api/people/1/')
+    axios.get('https://swapi.dev/api/planets/')    
+      .then(response => {
+//        console.log(response.data.post);
+        console.log(response.data);
+        dispatch({
+          type: SET_RECENT_POSTS,
+          payload: response.data
+        })
+    });
   }
 }
